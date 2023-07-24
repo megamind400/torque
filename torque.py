bdbe6effb377d28662688a816863c6ec1d26b4cd
mamad <megamind741074107410@gmail.com>
2023-07-25 01:50:58 +0330

    turn everything into fractions

--- torque.py
+++ torque.py
@@ -1,14 +1,15 @@
-#import decimal
+# import decimal
+from fractions import Fraction
+
 
 def echelon(mat):
     # mat = [[0, 3, 0, -1], [5, 2, -8, 8], [-4, 5, 9, -9]]
-    
 
     pi = 0
     pj = 0
     columns = len(mat[pi])
     rows = len(mat)
-    #mat = [[decimal.Decimal(x) for x in row] for row in mat]
+    # mat = [[decimal.Decimal(x) for x in row] for row in mat]
     while True:
         flag = False
         for i in range(len(mat)):
@@ -23,7 +24,7 @@ def echelon(mat):
                 break
 
         # scale the pivot to one:
-        mat[pi] = [i / mat[pi][pi] for i in mat[pi]]
+        mat[pi] = [Fraction(i, mat[pi][pi]) for i in mat[pi]]
 
         # turn everything under pivot to 0
         for x in range(1, rows):
@@ -32,35 +33,33 @@ def echelon(mat):
                     continue
                 else:
                     multiplier = mat[pi + x][pj]
-                    mat[pi + x] = [ mat[pi + x][i] - (mat[pi][i] * multiplier)  for i in range(columns)]
+                    mat[pi + x] = [mat[pi + x][i] - Fraction(mat[pi][i], Fraction(1, multiplier)) for i in
+                                   range(columns)]
 
         pi += 1
         pj += 1
-
+        # print (mat, f"\n")
         if pi >= rows or pj >= columns:
-            return mat #returned mat
+            return mat  # returned mat
 
     return mat
 
 
-decimal.setcontext(decimal.Context(prec=3))
+# decimal.setcontext(decimal.Context(prec=3))
 mat = [[-3, 2, 3, -11], [1, 2, -1, 9], [2, -4, -4, 8]]
 temp = echelon(mat)
 
-
-def rref(mat):
-    '''
-    parameter:
-        mat: must be echelon form
-
-    return:
-        mat: reduced row echelon form of the matrix
-    '''
-    [1,2,3,4]
-    [0,1,4,6]
-    []
-
+# def rref(mat):
+#     '''
+#     parameter:
+#         mat: must be echelon form
+#
+#     return:
+#         mat: reduced row echelon form of the matrix
+#     '''
+#     [1, 2, 3, 4]
+#     [0, 1, 4, 6]
+#     []
+#
 
 print(f"{temp[0]}\n{temp[1]}\n{temp[2]}")
-
-
